0698e7f27045b2055387e9219b4170e8e5565a80
Stuti Patel <stuti.patel@mailbox.tu-dreden.de>
2025-04-24 14:00:11 +0200

    heating demand for edges

--- app/core.py
+++ app/core.py
@@ -1,13 +1,18 @@
+from app.heating_demand_edges.assign_heating_demand_to_edges import assign_heating_demand_to_edges
 from app.minimization_problem.optimize_source import find_optimal_source
 from app.plotting_graph.color_code_node import color_nodes_based_on_assignment
-from app.plotting_graph.visualize_graph import visualize_heat_network
 from app.reading_input.create_graph import create_graph_from_xml
 
 def find_mass_flux(file_path, solver_name):
     G,sources,consumers,pos = create_graph_from_xml(file_path)
     # print("Graph created",G.number_of_nodes(), G.number_of_edges())
-    result=find_optimal_source(G,sources,consumers)
-    print(result[-1])
-    # visualize_heat_network(G,sources,consumers,result[-1])
+    result,heating_demand,assignments=find_optimal_source(G,sources,consumers)
+    print(assignments)
 
-    color_nodes_based_on_assignment(result[-1],G,sources,pos)
+    assign_heating_demand_to_edges(G,assignments,heating_demand)
+    return
+    # visualize_heat_network(G,sources,consumers,result[-1])
+    graph_name=str(file_path).split("/")[-1].replace(".vicus", "")
+    print(graph_name)
+
+    color_nodes_based_on_assignment(assignments,G,sources,pos,graph_name,heating_demand)
---
+++ app/heating_demand_edges/assign_heating_demand_to_edges.py
@@ -0,0 +1,4 @@
+def assign_heating_demand_to_edges(G,assignments,heating_demand):
+    print(G.nodes(data=True))
+
+    print(assignments)
